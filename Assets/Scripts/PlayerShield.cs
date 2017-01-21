48e284254cae53bee3541816601ebf85112a4b66
Marco Santarcangelo Zazzetta <marcosantar93@gmail.com>
2017-01-21 16:49:03 -0300

    Shields working

--- Assets/Scripts/PlayerShield.cs
+++ Assets/Scripts/PlayerShield.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShield : MonoBehaviour {
+public class PlayerShield : MonoBehaviour
+{
+    private PlayerLook playerLook;
     public float cooldownTime;
     public float shieldTime;
     private float delayTime=0;
     private bool shieldActivated=false;
+    public GameObject shieldContainer;
     public GameObject shieldObject;
     public int player = 0;
     private bool shieldCooldown=false;
@@ -17,12 +20,24 @@ public class PlayerShield : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         if (!shieldCooldown)
         {
+            if (gameObject.tag == "Player1")
+            {
+                playerLook = GameData.player1.GetComponent<PlayerLook>();
+            }
+            else if (gameObject.tag == "Player2")
+            {
+                playerLook = GameData.player2.GetComponent<PlayerLook>();
+            }
+
+            Vector3 delta = playerLook.lookVector;
+            shieldContainer.transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg));
             if (player == 1)
             {
-                if ((Input.GetKey(KeyCode.Comma)) && (!shieldActivated))
+                if ((Input.GetKey(KeyCode.Comma) || Input.GetButton("Joystick1Shield")) && (!shieldActivated))
                 {
                     shieldObject.SetActive(true);
                     shieldActivated = true;
@@ -31,7 +46,7 @@ public class PlayerShield : MonoBehaviour {
             }
             if (player == 2)
             {
-                if ((Input.GetKey(KeyCode.V)) && (!shieldActivated))
+                if ((Input.GetKey(KeyCode.V) || Input.GetButton("Joystick2Shield")) && (!shieldActivated))
                 {
                     shieldObject.SetActive(true);
                     shieldActivated = true;
