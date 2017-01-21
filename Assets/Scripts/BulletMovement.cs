52076d51da8106f34ad593d0ecc176c54a7d59ad
juan.rinaudo <jangamesdev@gmail.com>
2017-01-20 22:57:19 -0300

    Added bullet and bullet movement.

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+
+    public float movementSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(Time.deltaTime * movementSpeed, 0, 0));
+	}
+
+}
---
+++ Assets/Scripts/CharacterShoot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterShoot : MonoBehaviour {
+    
+    public GameObject bulletObject;
+    public GameObject bulletPool;
+    public GameObject shootPoint;
+    private List<GameObject> poolBullets;
+
+    public float timeToShoot;
+    private float shootTimer;
+
+	// Use this for initialization
+	void Start () {
+        poolBullets = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        shootTimer -= Time.deltaTime;
+        if(shootTimer <= 0)
+        {
+            GameObject bullet = getBullet();
+            bullet.active = true;
+            bullet.transform.parent = bulletPool.transform;
+            bullet.transform.position = shootPoint.transform.position;
+            bullet.transform.eulerAngles = transform.eulerAngles;
+            shootTimer += timeToShoot;
+        }
+	}
+
+    GameObject getBullet()
+    {
+        foreach(GameObject bullet in poolBullets)
+        {
+            if(!bullet.active)
+            {
+                return bullet;
+            }
+        }
+
+        GameObject newBullet = GameObject.Instantiate(bulletObject);
+        poolBullets.Add(newBullet);
+        return newBullet;
+    }
+
+}
